f7e040704976e2fcd3b090235d14e9e06f223109
Aristeu da Silva Filho <aristeusfilho@gmail.com>
2016-07-25 12:30:44 +0000

    Primeiro Commite

---
+++ config/express.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+module.exports = function() {
+  var app = express();
+
+  app.set('port', 3000);
+
+  app.use(express.static('./public'));
+
+  app.set('view engine', 'ejs');
+  app.set('views', './app/views');
+
+  return app;
+};
